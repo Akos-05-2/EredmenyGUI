1e87ed3d517a6e3bdde7f0b125f79d5582036a14
Olajkár Ákos <olajkar.akos.botond_2020c@bolyai-szakkozep.hu>
2024-04-18 09:59:47 +0200

    Add project files.

---
+++ EredmenyGUI/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EredmenyGUI
+{
+    public partial class EredmenyForm : Form
+    {
+        public EredmenyForm()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void EredmenyForm_Load(object sender, EventArgs e)
+        {
+            EredmenyRepositroy.Path = "eredmeny.txt";
+            dataGridView1.DataSource = EredmenyRepositroy.FindAll();
+        }
+    }
+}
